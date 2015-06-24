f83bffcdc7495cb3330c1d747c8fec9092abeb94
JeongJu Kim <haje01@gmail.com>
2015-06-24 11:13:55 +0900

    done

--- snippets/splash/main.py
+++ snippets/splash/main.py
@@ -1,8 +1,30 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+from kivy.clock import Clock
+
+SPLASH_TIME = 3
 
 
-class MyApp(App):
-    pass
+class Splash(Widget):
+    source = StringProperty(None)
+
+
+class SplashApp(App):
+    def build(self):
+        self.presplash = Splash(source='data/presplash.png')
+        self.splash = Splash(source='data/splash.png')
+        self.root.add_widget(self.presplash)
+        Clock.schedule_once(self.show_splash, SPLASH_TIME)
+
+    def show_splash(self, dt):
+        self.root.remove_widget(self.presplash)
+        self.root.add_widget(self.splash)
+        Clock.schedule_once(self.clear_splash, SPLASH_TIME)
+
+    def clear_splash(self, dt):
+        self.root.remove_widget(self.splash)
+
 
 if __name__ == '__main__':
-    MyApp().run()
+    SplashApp().run()
